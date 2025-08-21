dfad1f875b74ea63905bfbb8b78a277f9aec3fbc
Alexander Jackson <alexanderjackson@protonmail.com>
2025-08-21 07:39:07 +0100

    feat: provide the layer entirely

--- apps/foundation/telemetry/src/lib.rs
+++ apps/foundation/telemetry/src/lib.rs
@@ -1,13 +1,24 @@
+use std::borrow::Cow;
+
 use color_eyre::eyre::Result;
+use opentelemetry::trace::TracerProvider;
 use opentelemetry::Value;
 use opentelemetry_otlp::{Protocol, SpanExporter, WithExportConfig};
-use opentelemetry_sdk::trace::SdkTracerProvider;
+use opentelemetry_sdk::trace::{SdkTracer, SdkTracerProvider};
 use opentelemetry_sdk::Resource;
+use tracing_core::Subscriber;
+use tracing_opentelemetry::OpenTelemetryLayer;
+use tracing_subscriber::registry::LookupSpan;
 
-pub fn get_tracer_provider<S: Into<Value>>(
+pub fn get_tracer_provider<S, L>(
     service: S,
     endpoint: &str,
-) -> Result<SdkTracerProvider> {
+) -> Result<OpenTelemetryLayer<L, SdkTracer>>
+where
+    S: Into<Value> + Copy,
+    S: Into<Cow<'static, str>>,
+    L: Subscriber + for<'span> LookupSpan<'span>,
+{
     let exporter = SpanExporter::builder()
         .with_http()
         .with_protocol(Protocol::HttpBinary)
@@ -21,5 +32,7 @@ pub fn get_tracer_provider<S: Into<Value>>(
         .with_batch_exporter(exporter)
         .build();
 
-    Ok(provider)
+    let layer = OpenTelemetryLayer::new(provider.tracer(service));
+
+    Ok(layer)
 }
