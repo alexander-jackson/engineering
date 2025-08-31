0dfd8027f3aadc67695ec8373bc22fd95f9938fc
Alexander Jackson <alexanderjackson@protonmail.com>
2025-08-31 11:42:45 +0100

    fix: reduce type complexity

--- apps/foundation/logging/src/lib.rs
+++ apps/foundation/logging/src/lib.rs
@@ -5,14 +5,14 @@ use tracing_subscriber::layer::{Layered, SubscriberExt};
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{EnvFilter, Registry};
 
+type RegistryLayer = Layered<Layer<Registry>, Registry>;
+
 pub fn install_default_registry() {
     get_default_registry().init();
 }
 
-pub fn get_default_registry() -> Layered<
-    EnvFilter,
-    Layered<ErrorLayer<Layered<Layer<Registry>, Registry>>, Layered<Layer<Registry>, Registry>>,
-> {
+pub fn get_default_registry()
+-> Layered<EnvFilter, Layered<ErrorLayer<RegistryLayer>, RegistryLayer>> {
     let fmt_layer = tracing_subscriber::fmt::layer();
     let error_layer = ErrorLayer::default();
     let env_filter_layer = EnvFilter::builder()
