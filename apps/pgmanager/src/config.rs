8d25c04d65c9d83f7e60955a36db5026d7f14914
Alexander Jackson <alexanderjackson@protonmail.com>
2025-09-18 19:41:22 +0100

    feat: add `pgmanager`
    `pgbackup` has served well, but we probably need something a little more
    complex. Let's import the existing code and rename it to `pgmanager` so
    we can add more functionality to it.
    
    This change:
    * Adds the initial setup and imports the code
    * Makes it easier to run locally now that we have more complex
      configuration support

---
+++ apps/pgmanager/src/config.rs
@@ -0,0 +1,53 @@
+use std::path::PathBuf;
+
+use chrono::NaiveTime;
+use foundation_configuration::Secret;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Configuration {
+    pub backup_location: BackupLocation,
+    pub backup_schedule: BackupSchedule,
+    pub database: DatabaseConfig,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type", rename_all = "lowercase")]
+pub enum BackupLocation {
+    S3 { bucket: String },
+    Filesystem { root: PathBuf },
+}
+
+#[derive(Deserialize)]
+#[serde(tag = "type", rename_all = "lowercase")]
+pub enum BackupSchedule {
+    Oneshot,
+    Daily { time: NaiveTime },
+}
+
+#[derive(Deserialize)]
+pub struct DatabaseConfig {
+    pub username: String,
+    pub password: Option<Secret<String>>,
+    pub database: String,
+    pub host: String,
+    pub port: u16,
+}
+
+impl From<&DatabaseConfig> for tokio_postgres::Config {
+    fn from(value: &DatabaseConfig) -> Self {
+        let mut config = tokio_postgres::Config::new();
+
+        config
+            .user(value.username.clone())
+            .dbname(value.database.clone())
+            .host(value.host.clone())
+            .port(value.port);
+
+        if let Some(password) = value.password.as_deref() {
+            config.password(password);
+        }
+
+        config
+    }
+}
---
+++ apps/pgmanager/src/utils.rs
@@ -0,0 +1,78 @@
+use std::cmp::Ordering;
+use std::io::Write;
+use std::time::Duration;
+
+use chrono::NaiveTime;
+use color_eyre::eyre::Result;
+use flate2::write::GzEncoder;
+use flate2::Compression;
+
+#[tracing::instrument(skip(content))]
+pub fn compress(content: &[u8]) -> Result<Vec<u8>> {
+    let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(content)?;
+    let compressed = encoder.finish()?;
+
+    let formatter = humansize::make_format(humansize::BINARY);
+
+    tracing::info!(
+        input_size = formatter(content.len()),
+        output_size = formatter(compressed.len()),
+        "compressed some data using gzip"
+    );
+
+    Ok(compressed)
+}
+
+pub fn get_initial_offset(now: NaiveTime, schedule_time: NaiveTime) -> Duration {
+    match now.cmp(&schedule_time) {
+        Ordering::Equal => Duration::from_secs(0),
+        Ordering::Less => {
+            let secs = (schedule_time - now).num_seconds();
+            Duration::from_secs(secs as u64)
+        }
+        Ordering::Greater => {
+            let secs = 86400 - (now - schedule_time).num_seconds();
+            Duration::from_secs(secs as u64)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+
+    use chrono::NaiveTime;
+
+    use crate::utils::get_initial_offset;
+
+    #[test]
+    fn computes_no_offset_when_running_at_schedule_time() {
+        let now = NaiveTime::from_hms_opt(22, 30, 0).unwrap();
+        let schedule_time = now;
+
+        let offset = get_initial_offset(now, schedule_time);
+
+        assert_eq!(offset, Duration::from_secs(0));
+    }
+
+    #[test]
+    fn computes_correct_offset_when_running_before_schedule_time() {
+        let now = NaiveTime::from_hms_opt(22, 0, 0).unwrap();
+        let schedule_time = NaiveTime::from_hms_opt(22, 30, 0).unwrap();
+
+        let offset = get_initial_offset(now, schedule_time);
+
+        assert_eq!(offset, Duration::from_secs(30 * 60));
+    }
+
+    #[test]
+    fn computes_correct_offset_when_running_after_schedule_time() {
+        let now = NaiveTime::from_hms_opt(22, 45, 0).unwrap();
+        let schedule_time = NaiveTime::from_hms_opt(22, 30, 0).unwrap();
+
+        let offset = get_initial_offset(now, schedule_time);
+
+        assert_eq!(offset, Duration::from_secs(23 * 60 * 60 + 45 * 60));
+    }
+}
