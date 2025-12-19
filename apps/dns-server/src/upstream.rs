5e6483c298428dee2b38ae0388a39c351fd5f4f6
Alexander Jackson <alexanderjackson@protonmail.com>
2025-12-19 17:08:48 +0000

    feat: allow protocol to be configured (#34)
    Before, it was very difficult to test the `UpstreamResolver` since it
    expected to do DNS over HTTPS which would have been annoying to set up
    locally. Instead, let's allow passing the protocol here so we can use
    UDP in tests.
    
    This change:
    * Makes the protocol part of the configuration
    * Adds a test to make sure the upstream resolver works

--- apps/dns-server/src/upstream.rs
+++ apps/dns-server/src/upstream.rs
@@ -4,7 +4,6 @@ use std::time::Duration;
 
 use color_eyre::eyre::{eyre, Context, Result};
 use hickory_proto::op::{Message, MessageType, OpCode};
-use hickory_proto::xfer::Protocol;
 use hickory_resolver::config::{NameServerConfig, ResolverConfig, ResolverOpts};
 use hickory_resolver::name_server::TokioConnectionProvider;
 use hickory_resolver::{ResolveError, Resolver};
@@ -38,7 +37,7 @@ impl UpstreamResolver {
             "resolved upstream DNS server"
         );
 
-        let mut nameserver = NameServerConfig::new(upstream_addr, Protocol::Https);
+        let mut nameserver = NameServerConfig::new(upstream_addr, config.protocol);
         nameserver.tls_dns_name = Some(config.resolver.clone());
 
         let mut resolver_config = ResolverConfig::new();
@@ -101,5 +100,71 @@ impl UpstreamResolver {
         );
 
         Ok(response)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::net::{IpAddr, Ipv4Addr, SocketAddrV4};
+    use std::str::FromStr;
+
+    use color_eyre::eyre::Result;
+    use dns_mock_server::Server;
+    use hickory_proto::op::{Message, Query};
+    use hickory_proto::rr::record_type::RecordType;
+    use hickory_proto::rr::DNSClass;
+    use hickory_proto::xfer::Protocol;
+    use hickory_resolver::Name;
+    use tokio::net::UdpSocket;
+
+    use crate::config::UpstreamConfig;
+    use crate::upstream::UpstreamResolver;
+
+    #[tokio::test]
+    async fn can_resolve_upstream_records() -> Result<()> {
+        let upstream_addr = Ipv4Addr::new(93, 184, 216, 34);
+
+        let mut mock_server = Server::default();
+        mock_server.add_records("example.com.", vec![IpAddr::V4(upstream_addr)])?;
+
+        let socket = UdpSocket::bind(SocketAddrV4::new(Ipv4Addr::LOCALHOST, 0)).await?;
+        let addr = socket.local_addr()?;
+
+        tokio::spawn(async move {
+            mock_server.start(socket).await.unwrap();
+        });
+
+        let config = UpstreamConfig {
+            resolver: addr.ip().to_string(),
+            port: addr.port(),
+            protocol: Protocol::Udp,
+            timeout_seconds: 5,
+        };
+
+        let resolver = UpstreamResolver::new(&config).await?;
+
+        let name = Name::from_str("example.com.")?;
+        let query_type = RecordType::A;
+        let query_class = DNSClass::IN;
+
+        let mut query = Query::new();
+        query.set_name(name);
+        query.set_query_type(query_type);
+        query.set_query_class(query_class);
+
+        let mut message = Message::new();
+        message.add_query(query);
+
+        let resolved = resolver.resolve(&message).await?;
+
+        let answers: Vec<_> = resolved
+            .answers()
+            .iter()
+            .filter_map(|record| record.clone().into_data().into_a().ok().map(|a| a.0))
+            .collect();
+
+        assert_eq!(answers, vec![upstream_addr]);
+
+        Ok(())
     }
 }
