78d10dc9aab34bd8be9d878976e6f1c29c1eca9e
Alexander Jackson <alexanderjackson@protonmail.com>
2025-12-26 17:19:41 +0100

    feat: add `pgmanager` (#38)
    `pgbackup` has served well, but we probably need something a little more
    complex. Let's import the existing code and rename it to `pgmanager` so
    we can add more functionality to it.
    
    This change:
    * Adds the initial setup and imports the code
    * Makes it easier to run locally now that we have more complex
    configuration support

---
+++ apps/pgmanager/src/databases.rs
@@ -0,0 +1,60 @@
+use std::process::Stdio;
+
+use color_eyre::eyre::{eyre, Context, Result};
+use tokio::process::Command;
+use tokio_postgres::Client;
+
+use crate::config::DatabaseConfig;
+
+#[tracing::instrument(skip(client), ret)]
+pub async fn discover(client: &Client) -> Result<Vec<String>> {
+    let query = r#"
+        SELECT datname
+        FROM pg_database
+        WHERE datname NOT IN (
+            'postgres',
+            'template0',
+            'template1'
+        )
+    "#;
+
+    let rows = client.query(query, &[]).await?;
+    let databases = rows.into_iter().map(|row| row.get(0)).collect();
+
+    tracing::info!(?databases, "discovered some targets for backup");
+
+    Ok(databases)
+}
+
+#[tracing::instrument(skip(config))]
+pub async fn dump(config: &DatabaseConfig, database: &str) -> Result<Vec<u8>> {
+    let mut command = Command::new("pg_dump");
+
+    command
+        .args([
+            "-h",
+            &config.host,
+            "-p",
+            &config.port.to_string(),
+            "-d",
+            database,
+            "-U",
+            &config.username,
+        ])
+        .stdout(Stdio::piped());
+
+    if let Some(password) = config.password.as_deref() {
+        command.env("PGPASSWORD", password);
+    }
+
+    let stdout = command
+        .spawn()
+        .wrap_err_with(|| eyre!("failed to run `pg_dump` command"))?
+        .wait_with_output()
+        .await?
+        .stdout;
+
+    tracing::info!(bytes = %stdout.len(), "got some output from the dump");
+
+    Ok(stdout)
+}
---
+++ apps/pgmanager/src/main.rs
@@ -0,0 +1,100 @@
+use std::time::Duration;
+
+use aws_config::BehaviorVersion;
+use aws_sdk_s3::primitives::ByteStream;
+use chrono::Utc;
+use color_eyre::eyre::Result;
+use tokio::time::Instant;
+use tokio_postgres::NoTls;
+
+mod config;
+mod databases;
+mod utils;
+
+use crate::config::{BackupLocation, BackupSchedule, Configuration, DatabaseConfig};
+use crate::databases::{discover, dump};
+use crate::utils::{compress, get_initial_offset};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+    let config = foundation_init::run::<Configuration>()?;
+
+    let sdk_config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+    let s3_client = aws_sdk_s3::Client::new(&sdk_config);
+
+    match config.backup_schedule {
+        BackupSchedule::Oneshot => {
+            take_backups(&s3_client, &config.backup_location, &config.database).await?
+        }
+        BackupSchedule::Daily { time } => {
+            let offset = get_initial_offset(Utc::now().time(), time);
+            tracing::info!(?offset, %time, "waiting some time before running the first backups");
+
+            let start = Instant::now() + offset;
+            let period = Duration::from_secs(60 * 60 * 24);
+            let mut interval = tokio::time::interval_at(start, period);
+
+            loop {
+                interval.tick().await;
+                take_backups(&s3_client, &config.backup_location, &config.database).await?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+async fn take_backups(
+    s3_client: &aws_sdk_s3::Client,
+    backup_location: &BackupLocation,
+    database_config: &DatabaseConfig,
+) -> Result<()> {
+    let date = Utc::now().format("%Y-%m-%d");
+
+    let span = tracing::info_span!("backup", %date);
+    let _guard = span.enter();
+
+    let (client, connection) = tokio_postgres::Config::from(database_config)
+        .connect(NoTls)
+        .await?;
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    let databases = discover(&client).await?;
+
+    for database in databases {
+        let dump = dump(database_config, &database).await?;
+        let compressed = compress(&dump)?;
+
+        let key = format!("{database}/{database}.{date}.sql.gz");
+
+        match backup_location {
+            BackupLocation::S3 { bucket } => {
+                s3_client
+                    .put_object()
+                    .bucket(bucket)
+                    .key(&key)
+                    .body(ByteStream::from(compressed))
+                    .send()
+                    .await?;
+            }
+            BackupLocation::Filesystem { root } => {
+                let path = root.join(&key);
+
+                if let Some(parent) = path.parent() {
+                    tokio::fs::create_dir_all(parent).await?;
+                }
+
+                tokio::fs::write(path, compressed).await?;
+            }
+        }
+
+        tracing::info!(location = ?backup_location, %key, "persisted a backup");
+    }
+
+    Ok(())
+}
