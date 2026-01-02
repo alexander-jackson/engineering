2ae7978f7ffe080ab915a23abe86539e25dc14f0
Alexander Jackson <alexanderjackson@protonmail.com>
2026-01-02 14:03:43 +0000

    feat: allow access from work laptop (#49)
    Currently, instances can only be accessed from my personal laptop but it
    would be good to allow access from my work one as well. Let's bring one
    up that only allows access from the work one and then try and get both
    working.
    
    This change:
    * Brings up the `secondary` instance
    * Bumps the version of `vector` and downgrades `f2`
    * Adds my work SSH key

--- infrastructure/terraform/aws.tf
+++ infrastructure/terraform/aws.tf
@@ -294,10 +294,15 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 }
 
-# SSH key definition
+# SSH key definitions
 resource "aws_key_pair" "main" {
   key_name   = "macbook-m2-pro"
   public_key = file("./keys/id_rsa.pub")
+}
+
+resource "aws_key_pair" "work" {
+  key_name   = "macbook-m1-max"
+  public_key = file("./keys/work.pub")
 }
 
 module "primary" {
@@ -335,6 +340,47 @@ module "primary" {
   }
 
   key_name = aws_key_pair.main.key_name
+  hosted_zones = [
+    aws_route53_zone.opentracker.id,
+    aws_route53_zone.forkup.id
+  ]
+}
+
+module "secondary" {
+  source = "./modules/f2-instance"
+  name   = "secondary"
+
+  instance = {
+    type      = "t2.micro"
+    ami       = "ami-0ab14756db2442499"
+    vpc_id    = aws_vpc.main.id
+    subnet_id = aws_subnet.main.id
+  }
+
+  configuration = {
+    bucket    = module.config_bucket.name
+    key       = "f2/config.yaml"
+    image_tag = "20250829-1918"
+  }
+
+  logging = {
+    bucket     = module.logging_bucket.name
+    vector_tag = "0.52.0-alpine"
+  }
+
+  backups = {
+    bucket = module.postgres_backups_bucket.name
+  }
+
+  hackathon = {
+    bucket = module.hackathon_bucket.name
+  }
+
+  alerting = {
+    topic_arn = aws_sns_topic.outages.arn
+  }
+
+  key_name = aws_key_pair.work.key_name
   hosted_zones = [
     aws_route53_zone.opentracker.id,
     aws_route53_zone.forkup.id
@@ -405,6 +451,16 @@ resource "aws_security_group_rule" "allow_inbound_connections_to_postgres_from_p
   to_port                  = 5432
   protocol                 = "tcp"
   source_security_group_id = module.primary.security_group_id
+  security_group_id        = module.postgres.security_group_id
+}
+
+resource "aws_security_group_rule" "allow_inbound_connections_to_postgres_from_secondary" {
+  description              = format("Allow inbound connections from %s", module.secondary.security_group_id)
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  source_security_group_id = module.secondary.security_group_id
   security_group_id        = module.postgres.security_group_id
 }
 
