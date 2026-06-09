9fbce14d16c1ca79e15385262dcf377aa4be1075
Alexander Jackson <alexanderjackson@protonmail.com>
2026-06-09 20:27:46 +0100

    feat: teardown old instance (#140)
    This is no longer being used as all the traffic has been swapped over to
    the new one.
    
    This change:
    * Removes the instance

--- infrastructure/terraform/aws.tf
+++ infrastructure/terraform/aws.tf
@@ -329,41 +329,6 @@ module "primary" {
   inbound_http_subnet_id = aws_subnet.main.id
 }
 
-module "secondary" {
-  source = "./modules/f2-instance"
-  name   = "primsecondary"
-
-  instance = {
-    type      = "t4g.micro"
-    ami       = "ami-0b583f82e876e016c"
-    vpc_id    = aws_vpc.main.id
-    subnet_id = aws_subnet.main.id
-  }
-
-  configuration = {
-    bucket    = module.config_bucket.name
-    key       = "f2/config.yaml"
-    image_tag = "20260530-1116"
-  }
-
-  logging = {
-    bucket     = module.logging_bucket.name
-    vector_tag = "0.55.0-alpine"
-  }
-
-  hackathon = {
-    bucket = module.hackathon_bucket.name
-  }
-
-  extra_ebs_volume = {
-    size_gb     = 2
-    device_name = "/dev/sdf"
-  }
-
-  key_name               = aws_key_pair.main.key_name
-  inbound_http_subnet_id = aws_subnet.main.id
-}
-
 resource "aws_eip" "dns_server" {
   domain = "vpc"
 
@@ -499,16 +464,6 @@ resource "aws_security_group_rule" "primary_to_postgres" {
   to_port                  = 5432
   protocol                 = "tcp"
   source_security_group_id = module.primary.security_group_id
-  security_group_id        = module.rds_postgres.security_group_id
-}
-
-resource "aws_security_group_rule" "secondary_to_postgres" {
-  description              = "Allow inbound PostgreSQL from secondary"
-  type                     = "ingress"
-  from_port                = 5432
-  to_port                  = 5432
-  protocol                 = "tcp"
-  source_security_group_id = module.secondary.security_group_id
   security_group_id        = module.rds_postgres.security_group_id
 }
 
