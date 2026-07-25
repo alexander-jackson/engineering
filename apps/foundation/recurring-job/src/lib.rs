b0abd798df13dd97b20cc23bfbd44df186f3e1e5
Alexander Jackson <alexanderjackson@protonmail.com>
2026-07-25 07:26:28 +0100

    feat: support daily jobs
    Some jobs will want to run on a schedule (such as every 5 minutes)
    whereas others will want to run at a particular time each day (such as
    10pm when the traffic is lower).
    
    This change:
    * Adds support for daily jobs
    * Adds tests to make sure it works

--- apps/foundation/recurring-job/src/lib.rs
+++ apps/foundation/recurring-job/src/lib.rs
@@ -1,7 +1,8 @@
 use std::future::Future;
 use std::time::Duration;
 
-use color_eyre::eyre::Result;
+use chrono::{DateTime, TimeZone, Utc};
+use color_eyre::eyre::{Result, eyre};
 use foundation_shutdown::{CancellationToken, GracefulTask};
 
 #[derive(Copy, Clone, Debug)]
@@ -40,13 +41,9 @@ impl<T: Job> RecurringJob<T> {
     }
 }
 
-impl<T: Job> GracefulTask for RecurringJob<T> {
-    async fn run_until_shutdown(self, shutdown: CancellationToken) -> Result<()> {
-        let Schedule::Interval(duration) = self.state.schedule() else {
-            unimplemented!("only interval scheduling is supported for now");
-        };
-
-        let mut interval = tokio::time::interval(duration);
+impl<T: Job> RecurringJob<T> {
+    async fn run_interval_job(self, interval: Duration, shutdown: CancellationToken) -> Result<()> {
+        let mut interval = tokio::time::interval(interval);
         let job = T::NAME;
 
         loop {
@@ -65,6 +62,92 @@ impl<T: Job> GracefulTask for RecurringJob<T> {
 
         Ok(())
     }
+
+    async fn run_daily_job(
+        self,
+        hour: u32,
+        minute: u32,
+        shutdown: CancellationToken,
+    ) -> Result<()> {
+        let job = T::NAME;
+
+        loop {
+            let now = Utc::now();
+            let sleep_duration = calculate_sleep_duration(now, hour, minute)?;
+
+            tracing::info!(%job, ?sleep_duration, "waiting until next scheduled run");
+
+            tokio::select! {
+                _ = shutdown.cancelled() => {
+                    tracing::info!(%job, "shutting down gracefully");
+                    break;
+                }
+                _ = tokio::time::sleep(sleep_duration) => {
+                    if let Err(e) = self.state.run().await {
+                        tracing::warn!(%job, error = ?e, "job execution failed");
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl<T: Job> GracefulTask for RecurringJob<T> {
+    async fn run_until_shutdown(self, shutdown: CancellationToken) -> Result<()> {
+        match self.state.schedule() {
+            Schedule::Interval(duration) => {
+                tracing::info!(job = T::NAME, ?duration, "starting recurring job");
+                self.run_interval_job(duration, shutdown).await
+            }
+            Schedule::Daily { hour, minute } => {
+                tracing::info!(job = T::NAME, ?hour, ?minute, "starting recurring job");
+                self.run_daily_job(hour, minute, shutdown).await
+            }
+        }
+    }
+}
+
+fn calculate_next_run(
+    now: DateTime<Utc>,
+    target_hour: u32,
+    target_minute: u32,
+) -> Result<DateTime<Utc>> {
+    let current_date = now.date_naive();
+    let target_time = current_date
+        .and_hms_opt(target_hour, target_minute, 0)
+        .ok_or_else(|| eyre!("Invalid target hour or minute"))?;
+
+    // If the target time has already passed today, schedule for tomorrow
+    let next_run = if now.naive_utc() >= target_time {
+        current_date
+            .succ_opt()
+            .ok_or_else(|| eyre!("Failed to calculate next day"))?
+            .and_hms_opt(target_hour, target_minute, 0)
+            .ok_or_else(|| eyre!("Invalid target hour or minute for next day"))?
+    } else {
+        target_time
+    };
+
+    let next_run_utc = Utc
+        .from_local_datetime(&next_run)
+        .single()
+        .ok_or_else(|| eyre!("Failed to convert next run time to UTC"))?;
+
+    Ok(next_run_utc)
+}
+
+fn calculate_sleep_duration(
+    now: DateTime<Utc>,
+    target_hour: u32,
+    target_minute: u32,
+) -> Result<Duration> {
+    let next_run = calculate_next_run(now, target_hour, target_minute)?;
+    let duration_until_next_run = next_run - now;
+    let duration_until_next_run = duration_until_next_run.to_std().unwrap_or_default();
+
+    Ok(duration_until_next_run)
 }
 
 #[cfg(test)]
@@ -74,8 +157,10 @@ mod tests {
     use std::sync::atomic::{AtomicU32, Ordering};
     use std::time::Duration;
 
+    use chrono::{DateTime, Utc};
     use color_eyre::eyre::Result;
     use foundation_shutdown::{CancellationToken, GracefulTask};
+    use test_case::test_case;
 
     use crate::{Job, RecurringJob, Schedule};
 
@@ -101,7 +186,7 @@ mod tests {
     }
 
     #[tokio::test(start_paused = true)]
-    async fn can_create_and_run_recurring_job() {
+    async fn can_handle_scheduled_jobs() -> Result<()> {
         let counter = Arc::new(AtomicU32::new(0));
 
         let interval = Duration::from_millis(1);
@@ -117,9 +202,7 @@ mod tests {
         let shutdown_token = CancellationToken::new();
         let job_token = shutdown_token.clone();
 
-        tokio::spawn(async move {
-            job.run_until_shutdown(job_token).await.unwrap();
-        });
+        let handle = tokio::spawn(async move { job.run_until_shutdown(job_token).await });
 
         // advance time to allow the job to run a few times
         let iterations = 3;
@@ -128,5 +211,74 @@ mod tests {
         assert_eq!(counter.load(Ordering::SeqCst), iterations);
 
         shutdown_token.cancel();
+
+        handle.await??;
+
+        Ok(())
+    }
+
+    #[tokio::test(start_paused = true)]
+    async fn can_handle_daily_jobs() -> Result<()> {
+        let counter = Arc::new(AtomicU32::new(0));
+
+        let schedule = Schedule::daily(9, 0);
+
+        let job = TestJob {
+            counter: counter.clone(),
+            schedule,
+        };
+
+        let job = RecurringJob::new(job);
+
+        let shutdown_token = CancellationToken::new();
+        let job_token = shutdown_token.clone();
+
+        let handle = tokio::spawn(async move { job.run_until_shutdown(job_token).await });
+
+        // advance time to allow the job to run
+        let sleep_duration = crate::calculate_sleep_duration(Utc::now(), 9, 0)?;
+        tokio::time::sleep(sleep_duration + Duration::from_millis(1)).await;
+
+        assert_eq!(counter.load(Ordering::SeqCst), 1);
+
+        shutdown_token.cancel();
+
+        handle.await??;
+
+        Ok(())
+    }
+
+    fn datetime(date: &str, time: &str) -> DateTime<Utc> {
+        let datetime_str = format!("{} {} +0000", date, time);
+
+        DateTime::parse_from_str(&datetime_str, "%d/%m/%Y %H:%M %z")
+            .expect("Failed to parse date and time")
+            .with_timezone(&Utc)
+    }
+
+    fn time(time: &str) -> (u32, u32) {
+        let parts: Vec<&str> = time.split(':').collect();
+
+        let hour = parts[0].parse::<u32>().expect("Failed to parse hour");
+        let minute = parts[1].parse::<u32>().expect("Failed to parse minute");
+
+        (hour, minute)
+    }
+
+    #[test_case(datetime("01/06/2026", "08:00"), datetime("01/06/2026", "09:00"), time("09:00"); "before scheduled time")]
+    #[test_case(datetime("01/06/2026", "08:59"), datetime("01/06/2026", "09:00"), time("09:00"); "just before scheduled time")]
+    #[test_case(datetime("01/06/2026", "09:01"), datetime("02/06/2026", "09:00"), time("09:00"); "just after scheduled time")]
+    #[test_case(datetime("01/06/2026", "10:00"), datetime("02/06/2026", "09:00"), time("09:00"); "after scheduled time")]
+    fn can_calculate_next_run(
+        now: DateTime<Utc>,
+        expected: DateTime<Utc>,
+        scheduled_time: (u32, u32),
+    ) {
+        let (scheduled_hour, scheduled_minute) = scheduled_time;
+
+        let next_run = crate::calculate_next_run(now, scheduled_hour, scheduled_minute)
+            .expect("Failed to calculate next run time");
+
+        assert_eq!(next_run, expected);
     }
 }
